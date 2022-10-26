ce3ef2a1f825a874dde3ad33dcb9e9bf270a9889
jjw73 <jjw73012@gmail.com>
2022-10-26 14:15:32 -0500

    Updated Service Class

---
+++ src/main/java/Login.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Login {
+    private JPanel loginForm;
+    private JLabel Label;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
