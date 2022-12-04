ec9816766d81b4e75368caedc417b9da5dc8c1ee
OperationNanomachineSon <starcraftxue@gmail.com>
2022-12-04 16:21:41 -0600

    UserDAO edit

--- src/main/java/UserDAO.java
+++ src/main/java/UserDAO.java
@@ -1,4 +1,8 @@
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDAO {
@@ -7,5 +11,23 @@ public class UserDAO {
 	static final String USER = "admin";
 	static final String PASS = "HelloWorld";
 	
+	public static User checkPassword(String un, String pass)
+	{
+		User u=null;
+		try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+				Statement stmt = conn.createStatement();){
+				ResultSet rs = stmt.executeQuery("SELECT password FROM Users WHERE username='"+un+'"');
+				// Extract data from result set
+				if (rs.next()) {
+					// Retrieve by column name
+					if(rs.getString("password").equals(pass))
+					{
+						u=new User(rs.getString("username"))
+					}
+				}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} 
+	}
 	
 }
