7f0ea3af23bb616a19db1fd18bb58f07bf79d3d0
Benjamin-Kilpatrick <befk500@gmail.com>
2022-12-05 12:12:05 -0600

    BusinessUI update

--- src/main/java/BusinessUI.java
+++ src/main/java/BusinessUI.java
@@ -1,11 +1,27 @@
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class BusinessUI extends JFrame {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class BusinessUI extends JFrame implements ActionListener{
 	
 	private static final long serialVersionUID = 6865092312557299097L;
 
 	public BusinessUI() {
 		super("Manage Business");
+		JPanel content = new JPanel();
+		JButton add = new JButton("Add");
+		add.addActionListener(this);
+		content.add(add);
+		this.setContentPane(content);
+		this.pack();
+		this.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
 		
 	}
 }
