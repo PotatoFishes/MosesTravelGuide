aaa9d3ddf68b318a0823b0c141912a518ed760ad
PotatoFishes <39881509+PotatoFishes@users.noreply.github.com>
2022-12-05 13:23:10 -0600

    Email works

--- src/main/java/SearchService.java
+++ src/main/java/SearchService.java
@@ -3,6 +3,7 @@ public class SearchService {
     private static String s;
     public static String getData(Object o) {
         s = "Stuff, One, Two, Three, four, five, six, seven, eight, nine, ten, eleven, twelve";
+
         if (o == "Location") {
             // return all locations
         } else if (o == "Services") {
